d3a1617d4260b1ac40a2887b03024e720287a89d
dlebansais <dlebansais@gmail.com>
2020-10-21 23:07:08 +0200

    Added support for balloon click handlers.

--- TaskbarTools/TaskbarBalloon.cs
+++ TaskbarTools/TaskbarBalloon.cs
@@ -1,6 +1,7 @@
 ﻿namespace TaskbarTools
 {
     using System;
+    using System.Collections.Generic;
     using System.Drawing;
     using System.Threading;
     using System.Windows.Forms;
@@ -10,17 +11,199 @@
     /// </summary>
     public static class TaskbarBalloon
     {
+        #region Init
+        static TaskbarBalloon()
+        {
+            DefaultDelay = TimeSpan.FromSeconds(5);
+        }
+
+        /// <summary>
+        /// Gets the default delay before a balloon closes.
+        /// </summary>
+        public static TimeSpan DefaultDelay { get; }
+        #endregion
+
+        #region Client Interface
         /// <summary>
         /// Display a notification in a taskbar balloon.
         /// </summary>
         /// <param name="text">The text to show.</param>
-        /// <param name="delayShow">The delay showing the balloon.</param>
-        /// <param name="delayWait">The delay waiting synchronously to ensure the balloon is entirely visible upon return.</param>
-        public static void Show(string text, TimeSpan delayShow, TimeSpan delayWait)
+        /// <param name="delay">The delay, in milliseconds.</param>
+        public static void Show(string text, TimeSpan delay)
         {
-            using NotifyIcon Notification = new NotifyIcon() { Visible = true, Icon = SystemIcons.Shield, Text = text, BalloonTipText = text };
-            Notification.ShowBalloonTip((int)delayShow.TotalMilliseconds);
+            Show(text, delay, TimeSpan.Zero);
+        }
+
+        /// <summary>
+        /// Display a notification in a taskbar balloon.
+        /// </summary>
+        /// <param name="text">The text to show.</param>
+        /// <param name="delay">The delay, in milliseconds.</param>
+        /// <param name="delayWait">The delay waiting synchronously to ensure the balloon is entirely visible upon return.</param>
+        public static void Show(string text, TimeSpan delay, TimeSpan delayWait)
+        {
+            using NotifyIcon Notification = new NotifyIcon() { Visible = true, Icon = SystemIcons.Shield, Text = ShortString(text), BalloonTipText = ShortString(text) };
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            BallonPrivateData Data = new BallonPrivateData(Notification);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+            Show(delay, Notification, Data);
+
             Thread.Sleep(delayWait);
         }
+
+        /// <summary>
+        /// Display a notification in a taskbar balloon.
+        /// </summary>
+        /// <param name="text">The text to show.</param>
+        /// <param name="delay">The delay, in milliseconds.</param>
+        /// <param name="clickHandler">Handler for the click event.</param>
+        /// <param name="clickData">Handler data for the click event.</param>
+        public static void Show(string text, TimeSpan delay, Action<object> clickHandler, object clickData)
+        {
+            NotifyIcon Notification = new NotifyIcon() { Visible = true, Icon = SystemIcons.Shield, Text = ShortString(text), BalloonTipText = ShortString(text) };
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            BallonPrivateData Data = new BallonPrivateData(Notification, clickHandler, clickData);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+            Show(delay, Notification, Data);
+        }
+        #endregion
+
+        #region Implementation
+        private static string? ShortString(string? text)
+        {
+            if (text != null && text.Length >= 16)
+                return text.Substring(0, 8) + "..." + text.Substring(text.Length - 8, 8);
+            else
+                return text;
+        }
+
+        private static void Show(TimeSpan delay, NotifyIcon notification, BallonPrivateData data)
+        {
+            try
+            {
+                notification.Tag = data;
+                notification.BalloonTipClosed += new EventHandler(OnClosed);
+                notification.BalloonTipClicked += new EventHandler(OnClicked);
+                notification.Click += new EventHandler(OnClicked);
+                notification.MouseClick += new MouseEventHandler(OnMouseClicked);
+                notification.ShowBalloonTip((int)delay.TotalMilliseconds);
+                DisplayedBalloonList.Add(data);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+            }
+        }
+
+        private static List<BallonPrivateData> DisplayedBalloonList = new List<BallonPrivateData>();
+
+        private static void OnClosed(object sender, EventArgs e)
+        {
+            BallonCloseHandler(sender);
+        }
+
+        private static void OnClicked(object sender, EventArgs e)
+        {
+            BallonClickHandler(sender);
+            BallonCloseHandler(sender);
+        }
+
+        private static void OnMouseClicked(object sender, MouseEventArgs e)
+        {
+            BallonClickHandler(sender);
+            BallonCloseHandler(sender);
+        }
+
+        private static void BallonClickHandler(object sender)
+        {
+            if (sender is NotifyIcon Notification && Notification.Tag is BallonPrivateData Data)
+                if (Data.GetClickHandler(out Action<object> ClickHandler, out object ClickData))
+                    ClickHandler.Invoke(ClickData);
+        }
+
+        private static void BallonCloseHandler(object sender)
+        {
+            if (sender is NotifyIcon Notification)
+            {
+                Notification.Visible = false;
+
+                if (Notification.Tag is BallonPrivateData Data)
+                {
+                    Notification.Tag = null;
+                    DisplayedBalloonList.Remove(Data);
+                    Data.Closed();
+                    using BallonPrivateData CloseData = Data;
+                }
+
+                using NotifyIcon CloseNotification = Notification;
+            }
+        }
+
+        private class BallonPrivateData : IDisposable
+        {
+            public BallonPrivateData(NotifyIcon notification)
+            {
+                Notification = notification;
+                ClickData = this;
+            }
+
+            public BallonPrivateData(NotifyIcon notification, Action<object> clickHandler, object clickData)
+                : this(notification)
+            {
+                ClickHandler = clickHandler;
+                ClickData = clickData;
+            }
+
+            public NotifyIcon? Notification { get; private set; }
+            public Action<object>? ClickHandler { get; private set; }
+            public object ClickData { get; }
+            public bool IsClosed { get; private set; }
+
+            public void Closed()
+            {
+                IsClosed = true;
+            }
+
+            public bool GetClickHandler(out Action<object> clickHandler, out object clickData)
+            {
+                clickData = ClickData;
+
+                if (ClickHandler != null)
+                {
+                    clickHandler = ClickHandler;
+                    ClickHandler = null;
+                    return true;
+                }
+                else
+                {
+                    clickHandler = new Action<object>((object data) => { });
+                    return false;
+                }
+            }
+
+            protected virtual void Dispose(bool isDisposing)
+            {
+                if (isDisposing)
+                    DisposeNow();
+            }
+
+            private void DisposeNow()
+            {
+                Notification = null;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            ~BallonPrivateData()
+            {
+                Dispose(false);
+            }
+        }
+        #endregion
     }
 }
